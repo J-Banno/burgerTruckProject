6f8dfe369d664ec90871a71327c152e472ef5c46
bannholtzer jonathan <bannholtzer.j@gmail.com>
2021-07-05 21:51:12 +0200

    section presentation

---
+++ client/src/components/presentationBlock/presentationBlock.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./presentationBlock.css";
+
+export default function PresentationBlock() {
+  return (
+    <section className="presBlock">
+      {/****** Block One ******/}
+
+      <div className="firstPresBlock">
+        <h1 className="titltePresentation">Burger Truck</h1>
+
+        <p className="textPresBlock">
+          Food-Truck spécialisé dans les hamburgers, Gilles à la cuisine et Elsa
+          à la vente s’évertuent à vous proposer une gamme d’hamburgers
+          gourmets.
+          <br />
+          <br />
+          Tous nos plats sont élaborés dans notre camion avec des ingrédientsf
+          frais et de qualité renouvelé chaque jour. A l’heure du déjeuner venez
+          déguster nos burger maisons.
+        </p>
+      </div>
+
+      {/* Image One */}
+      <div className="firstImgPresBlock imgPresBlock"></div>
+
+      {/****** Block Two ******/}
+      <div className="secondPresBlock ">
+        <p className="textPresBlock">
+          Chez Burger Truck nous respectons la chaîne du froid, et sommes équipé
+          pour convectionner vos commande dans des mesures d’hyiènes les plus
+          strictes.
+        </p>
+      </div>
+
+      {/* Image Two */}
+      <div className="secondImgPresBlock imgPresBlock"></div>
+
+      {/****** Block Third ******/}
+      <div className="thirdPresBlock">
+        <p className="textPresBlock">
+          Un espace détente est mis à votre disposition, pour profiter de vos
+          amis et savourer vos plats.
+        </p>
+      </div>
+
+      {/* Image Three */}
+
+      <div className="thirdImgPresBlock imgPresBlock"></div>
+    </section>
+  );
+}
--- client/src/containers/homePage/home.js
+++ client/src/containers/homePage/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/navbar";
 import Background from "../../components/background/background";
+import Presentation from "../../components/presentationBlock/presentationBlock";
 import Footer from "../../components/footer/footer";
 
 function HomePage() {
@@ -9,6 +10,7 @@ function HomePage() {
     <div>
       <Navbar />
       <Background />
+      <Presentation />
       <Footer />
     </div>
   );
